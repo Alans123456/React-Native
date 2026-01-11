db999b94661579a8f761a42e360df075c73da363
tysion <ctit23.ala@ismt.edu.np>
2026-01-11 22:35:36 +0545

    some problem in node module going to fix tommorow

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,24 @@
-import { api } from "@/convex/_generated/api";
-import useTheme, { ColorScheme } from "@/hooks/useTheme";
-import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import useTheme from "@/hooks/useTheme";
+import { LinearGradient } from "expo-linear-gradient";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
-  const todos = useQuery(api.todos.getTodos);
-  console.log(todos);
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearall = useMutation(api.todos.clearAll);
-  const styles = createstyles(colors);
+
+  const styles = createHomeStyles(colors);
   return (
-    <View style={styles.container}>
-      <Text>Finally success.</Text>
-      <TouchableOpacity style={{ padding: 20 }} onPress={toggleDarkMode}>
-        <Text>Toggle</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{ padding: 20 }}
-        onPress={() => addTodo({ text: "Need to practice more" })}
-      >
-        <Text>Add Todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{ padding: 20 }} onPress={() => clearall()}>
-        <Text>Clear All</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.safeArea}>
+        <Text>Finally success.</Text>
+        <TouchableOpacity style={{ padding: 20 }} onPress={toggleDarkMode}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const createstyles = (colors: ColorScheme) => {
-  const Styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: colors.bg,
-    },
-    text: {
-      color: colors.text,
-    },
-  });
-  return Styles;
-};
