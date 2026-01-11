25893434f953053555d2bdb6aea4e3514cb5f4bc
tysion <ctit23.ala@ismt.edu.np>
2026-01-11 21:12:29 +0545

    added the fix commmand in my database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,24 @@
 import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
   const todos = useQuery(api.todos.getTodos);
   console.log(todos);
+  const addTodo = useMutation(api.todos.addTodo);
   return (
     <View style={Styles.container}>
       <Text>Finally success.</Text>
       <TouchableOpacity style={{ padding: 20 }} onPress={toggleDarkMode}>
         <Text>Toggle</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ padding: 20 }}
+        onPress={() => addTodo({ text: "Need to practice more" })}
+      >
+        <Text>Add Todo</Text>
       </TouchableOpacity>
     </View>
   );
