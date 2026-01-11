60e1844f21ccd99d41639a66eec9224dfc3cfff2
tysion <ctit23.ala@ismt.edu.np>
2026-01-11 21:32:52 +0545

    import thr color from thr theme hook and implementing in the index by importing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,17 @@
 import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
   const todos = useQuery(api.todos.getTodos);
   console.log(todos);
   const addTodo = useMutation(api.todos.addTodo);
   const clearall = useMutation(api.todos.clearAll);
+  const styles = createstyles(colors);
   return (
-    <View style={Styles.container}>
+    <View style={styles.container}>
       <Text>Finally success.</Text>
       <TouchableOpacity style={{ padding: 20 }} onPress={toggleDarkMode}>
         <Text>Toggle</Text>
@@ -28,10 +29,17 @@ export default function Index() {
   );
 }
 
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const createstyles = (colors: ColorScheme) => {
+  const Styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.bg,
+    },
+    text: {
+      color: colors.text,
+    },
+  });
+  return Styles;
+};
