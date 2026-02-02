07ddeb7b5cd35d5bdcea3a14cbbe84e8a9058763
tysion <ctit23.ala@ismt.edu.np>
2026-02-02 22:09:54 +0545

    Add the gradent color flat list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,11 +3,14 @@ import Header from "@/components/Header";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
 import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
 import useTheme from "@/hooks/useTheme";
 import { useQuery } from "convex/react";
 import { LinearGradient } from "expo-linear-gradient";
-import { Text, TouchableOpacity } from "react-native";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+type Todo = Doc<"todos">;
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
@@ -18,6 +21,38 @@ export default function Index() {
   if (isLoading) {
     return <LoadingSpinner />;
   }
+  const rendeerTodoItem = ({ item }: { item: Todo }) => {
+    return (
+      <View style={styles.todoItemWrapper}>
+        <LinearGradient
+          colors={colors.gradients.surface}
+          style={styles.todoItem}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+        >
+          <TouchableOpacity
+            style={styles.checkbox}
+            activeOpacity={0.7}
+            onPress={() => {}}
+          >
+            <LinearGradient
+              colors={
+                item.isCompleted
+                  ? colors.gradients.success
+                  : colors.gradients.muted
+              }
+              style={[
+                styles.checkboxInner,
+                {
+                  borderColor: item.isCompleted ? "transparent" : colors.border,
+                },
+              ]}
+            ></LinearGradient>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    );
+  };
   return (
     <LinearGradient
       colors={colors.gradients.background}
@@ -26,9 +61,14 @@ export default function Index() {
       <SafeAreaView style={styles.safeArea}>
         <Header />
         <TodoInput />
-        {todos.map((todo) => (
-          <Text key={todo._id}>{todo.text}</Text>
-        ))}
+        <FlatList
+          data={todos}
+          keyExtractor={(item) => item._id}
+          renderItem={rendeerTodoItem}
+          style={styles.todoList}
+          contentContainerStyle={styles.todoListContent}
+        />
+
         <TouchableOpacity style={{ padding: 20 }} onPress={toggleDarkMode}>
           <Text>Toggle</Text>
         </TouchableOpacity>
